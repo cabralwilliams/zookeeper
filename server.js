9faebb253e001abc8cbb3f672317a9526d00415a
Cabral Williams <cabral.williams@gmail.com>
2021-10-22 14:17:01 -0500

    front end pages set up

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 const { animals } = require('./data/animals.json');
 console.log(animals);
 
@@ -130,6 +132,22 @@ app.post('/api/animals', (req,res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
